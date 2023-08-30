4fe69be502b1c192f96edf2118eaf310030462b5
Johnviti <84483840+Johnviti@users.noreply.github.com>
2023-08-30 18:23:56 -0300

    Novos testes

--- testes.py
+++ testes.py
@@ -1,39 +1,48 @@
-from Miniprojeto import eh_possivel_sair, mostrar
+from Miniprojeto import eh_possivel_sair
 from labirinto import gera_lab
 
-#Teste 1
+labirinto2 = [
+    ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+    [' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
+    ['#', '#', ' ', ' ', ' ', '#', '#', ' ', '#', '#'],
+    ['#', ' ', ' ', '#', '#', '#', '#', ' ', '#', ' '],
+    ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
+]
+
+resultado = eh_possivel_sair(labirinto2)
+
+print(resultado)
+
 labirinto1 = [
 
 ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-[' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', '#'],
-['#', ' ', ' ', ' ', '#', '#', ' ', ' ', ' ', '#'],
-['#', '#', '#', ' ', ' ', '#', ' ', ' ', '#', '#'],
-['#', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#'],
+['#', ' ', '#', '#', '#', '#', ' ', '#', ' ', '#'],
+['#', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', '#'],
+['#', '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' '],
 ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
 
-resultado_esperado1 = True
 resultado1 = eh_possivel_sair(labirinto1)
-print(resultado1 == resultado_esperado1) #deve ser True
 
-if resultado1:
-    print("Conseguimos achar uma saída: " + mostrar(labirinto1)) #é pra printar com o 'X' na casa (9,4)
+print(resultado1)
 
-#Teste 2
-labirinto2 = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-[' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', '#'],
-['#', '#', ' ', ' ', ' ', '#', '#', ' ', '#', '#'],
-['#', ' ', ' ', '#', '#', '#', '#', ' ', '#', ' '],
+print("'#', '#', '#', '#', '#', '#', '#', '#', '#', '#'")
+
+labirinto2 = [
+
+['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+[' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' '],
+['#', ' ', '#', '#', '#', '#', ' ', '#', ' ', ' '],
+['#', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' '],
+['#', '#', ' ', '#', ' ', ' ', ' ', '#', '#', '#'],
 ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
 
-resultado_esperado1 = False
-resultado1 = eh_possivel_sair(labirinto2)
-print(resultado1 == resultado_esperado1) #deve ser True
+resultado2 = eh_possivel_sair(labirinto2)
 
-if resultado1:
-    print(mostrar(labirinto2))
+print(resultado2)
 
+# labirinto = gera_lab(7,15)
 
-labirinto = gera_lab(7,15) #gerando um labirinto
-print(labirinto)
-resultadoTeste = eh_possivel_sair(labirinto)
-print(resultadoTeste)
+# resultadoTeste = eh_possivel_sair(labirinto)
+
+# print(resultadoTeste)

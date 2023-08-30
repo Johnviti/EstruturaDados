9d784edb82de580e7ac874ec1b6afcb67a0cb6ad
Johnviti <84483840+Johnviti@users.noreply.github.com>
2023-08-30 18:20:45 -0300

    Update Miniprojeto.py

--- Miniprojeto.py
+++ Miniprojeto.py
@@ -5,52 +5,35 @@ def eh_possivel_sair(labirinto):
     def esta_vazio(x, y):
         return 0 <= x < len(labirinto) and 0 <= y < len(labirinto[0]) and labirinto[x][y] == ' '
     
-    stack = Stack()  
-    stack.push((0, 0)) 
-    percorrido = []
+    stack = Stack()  # Usando a classe Stack
+    stack.push((0, 0))  # Início do labirintoirinto
+    percorrido = set()
     
     while not stack.is_empty():
-        x, y = stack.top()
-        
+        x, y = stack.pop()
         saida = (len(labirinto) - 2, len(labirinto[0]) - 1) #(m - 2, n - 1)
 
         if (x, y) in percorrido:
-            stack.pop()
-            percorrido.pop()
             continue
         
-        percorrido.append((x, y))
+        percorrido.add((x, y))
         
         if (x, y) == saida:
-            labirinto[x][y] = 'x'  
             return True  
         
         labirinto[x][y] = '+'  
 
-        #imprimi o labirinto
+        # Mostra o labirintoirinto com os passos atuais
         for linhas in labirinto:
             print(''.join(linhas))
         print()
         
         opcoes = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        proxima_opcao = None
         
-        for campoX, campoY in opcoes:
-            novo_x, novo_y = x + campoX, y + campoY
+        for dx, dy in opcoes:
+            new_x, new_y = x + dx, y + dy
             
-            if esta_vazio(novo_x, novo_y)and (novo_x, novo_y) not in percorrido:
-                proxima_opcao = (novo_x, novo_y)
-                break
-            
-        if proxima_opcao:
-            stack.push(proxima_opcao)
-        else:
-            stack.pop()
-            percorrido.pop()    
-        
-
+            if esta_vazio(new_x, new_y):
+                stack.push((new_x, new_y))
+    
     return False  # Não foi encontrada uma saída
-
-def mostrar(labirinto):
-    for linhas in labirinto:
-        print(''.join(linhas))
